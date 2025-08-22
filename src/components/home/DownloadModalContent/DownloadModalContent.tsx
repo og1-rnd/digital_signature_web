f3bf99e0544bf5e87f2088d63a6013a423f04dd3
evgdega <dorohov.e@og1.ru>
2025-08-22 18:32:39 +0700

    update links and images

--- src/components/home/DownloadModalContent/DownloadModalContent.tsx
+++ src/components/home/DownloadModalContent/DownloadModalContent.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './DownloadModalContent.scss';
 import { cn } from '@bem-react/classname';
-import GooglePlayPng from '@assets/img/google-play.png';
-import AppStorePng from '@assets/img/app-store.png';
+import GooglePlayPng from '@assets/img/play.png';
+import AppStorePng from '@assets/img/apple.png';
+import AppGalleryPng from '@assets/img/gallery.png';
 import AppText from '@ui-kit/typography';
 import { DownloadButton } from '@components/home';
 import { IDownloadButton } from '@components/home/DownloadButton/DownloadButton.types';
@@ -15,8 +16,12 @@ const CnDownloadModalContent = cn('download-modal-content');
  */
 const DownloadModalContent: React.FC = () => {
   const storeDownloadButtonItems: Array<IStoreDownloadModalContentButton> = [
-    { src: AppStorePng, link: 'https://apps.apple.com/app/%D1%8D%D1%86%D0%BF/id6446481549' },
+    {
+      src: AppStorePng,
+      link: 'https://apps.apple.com/ru/app/%D1%8D%D1%86%D0%BF-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D0%B3%D0%B8%D0%BC%D0%BD%D0%B0%D0%B7%D0%B8%D1%8F-1/id6749936481',
+    },
     { src: GooglePlayPng, link: 'https://play.google.com/store/apps/details?id=sign.og1.ru' },
+    { src: AppGalleryPng, link: 'https://appgallery.huawei.com/#/app/C115031305' },
   ];
 
   const downloadButtonItems: Array<IDownloadButton> = [
@@ -51,9 +56,12 @@ const DownloadModalContent: React.FC = () => {
     <div className={CnDownloadModalContent()}>
       <div className={CnDownloadModalContent('mobile-store-container')}>
         {storeDownloadButtonItems.map(({ src, link }) => (
-          <div onClick={() => openLink(link)}>
-            <img className={CnDownloadModalContent('mobile-store-image')} src={src} alt={''} />
-          </div>
+          <img
+            onClick={() => openLink(link)}
+            className={CnDownloadModalContent('mobile-store-image')}
+            src={src}
+            alt={''}
+          />
         ))}
       </div>
       <AppText text={'или'} fontStyle={'fontBody'} />
